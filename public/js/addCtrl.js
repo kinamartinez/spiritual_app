37e23347ffa350e89b2e1df07f875d27c43c894b
Karina <kinamartinez@gmail.com>
2018-05-31 11:12:52 +0300

    test

--- public/js/addCtrl.js
+++ public/js/addCtrl.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 // Creates the addCtrl Module and Controller. Note that it depends on the 'geolocation' module and service.
 const addCtrl = angular.module('addCtrl', ['geolocation']);
 addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, gservice){
@@ -26,7 +28,7 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
         $scope.formData.latitude = parseFloat(coords.lat).toFixed(3);
 
         // Display message confirming that the coordinates verified.
-        $scope.formData.htmlverified = "Yep (Thanks for giving us real data!)";
+        $scope.formData.htmlverified = 'Yep (Thanks for giving us real data!)';
 
         gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
 
@@ -34,13 +36,13 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
 
     // Functions
     // Get coordinates based on mouse click. When a click event is detected....
-    $rootScope.$on("clicked", function(){
+    $rootScope.$on('clicked', function(){
 
         // Run the gservice functions associated with identifying coordinates
         $scope.$apply(function(){
             $scope.formData.latitude = parseFloat(gservice.clickLat).toFixed(3);
             $scope.formData.longitude = parseFloat(gservice.clickLong).toFixed(3);
-            $scope.formData.htmlverified = "(Hey You are not in this location...)";
+            $scope.formData.htmlverified = '(Hey You are not in this location...)';
         });
     });
 
@@ -64,9 +66,9 @@ addCtrl.controller('addCtrl', function($scope, $http, $rootScope, geolocation, g
             .success(function (data) {
 
                 // Once complete, clear the form (except location)
-                $scope.formData.username = "";
-                $scope.formData.email = "";
-                $scope.formData.fullname= "";
+                $scope.formData.username = '';
+                $scope.formData.email = '';
+                $scope.formData.fullname = '';
 
                 gservice.refresh($scope.formData.latitude, $scope.formData.longitude);
 
